49f9972879d8e6a66f4a8568216f507bdc004944
Haoran Yan <haoranyan@ucsb.edu>
2025-11-15 20:51:31 -0500

    Updated Halo+Leo

--- src/main.py
+++ src/main.py
@@ -1,3 +1,82 @@
+# from typing import Tuple
+# from math import *
+# import numpy as np
+# import control as ct
+# from scipy.spatial.transform import Rotation as Rot
+# import plotTraj
+# from classes import *
+# from usePyVista import jwstVisualizationFixed 
+# from pathlib import Path
+
+# OUTPUT_DIR = Path("results/animation"); OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
+# # Instantiate Three body system and its compositions. 
+# TB = ThreeBodySystem()
+
+# # Set the initial states for JWST in Rotating frame and nondimensional unit
+# # Unstable Halo Orbit
+# TB.JamesWebb.X0 = np.array([1.0062010416592476,             # x
+#                             -9.8074369820712423e-16,       # xdot
+#                             -2.1523078518259630e-23,       # y
+#                             -1.3253477924660511e-2,        # ydot
+#                             1.2380311201349303e-2,        # z
+#                             -1.0956603459061133e-14])      # zdot
+
+# # Stable Halo Orbit
+# # TB.JamesWebb.X0 = np.array([1.0065075924336142E+0,             # x
+# #                             2.6228553664868397E-16,       # xdot
+# #                             -2.1016745594119651E-23,       # y
+# #                             -1.3729810632559413E-2,        # ydot
+# #                             1.2329298254423008E-2,        # z
+# #                             -2.4057058092832893E-15])      # zdot
+
+# # Define parameters used in JWST dynamics
+# JWST_params = {'pi_1':TB.pi_1,'pi_2':TB.pi_2}
+
+# # Define the Input/Output System
+# JWST_IO = ct.nlsys(TB.JamesWebb.JWST_update_nondim, TB.JamesWebb.JWST_output,
+#                     states=['x','xdot','y','ydot','z','zdot'], name = 'JWST',
+#                     inputs=['ux','uy','uz'], outputs=['x','xdot','y','ydot','z','zdot'],
+#                     params=JWST_params)
+
+# # Simulation
+# number_of_years = 2
+# timepts_year = 1000
+# timepts = np.linspace(0,number_of_years*TB.year,number_of_years*timepts_year)       # Nondimensinal time points
+# u = [np.zeros_like(timepts),np.zeros_like(timepts),np.zeros_like(timepts)]
+# response = ct.input_output_response(JWST_IO, timepts=timepts, inputs=u, initial_state=TB.JamesWebb.X0,
+#                                     solve_ivp_kwargs={'rtol':1e-9,'atol':1e-12}, solve_ivp_method='RK45')
+# time, outputs, inputs = response.time, response.outputs, response.inputs
+
+# # Retrive JWST trajectories in rotation frame and dimentionalize them
+# x = TB.r_12 * outputs['x']
+# y = TB.r_12 * outputs['y']
+# z = TB.r_12 * outputs['z']
+# xdot = TB.V_C * outputs['xdot']
+# ydot = TB.V_C * outputs['ydot']
+# zdot = TB.V_C * outputs['zdot']
+# t = TB.t_C * time
+
+# # Plot the JWST Trajectory
+# plotTraj.Plot_static_RF(x,y,z,t,TB.r_12,TB.x_L2,TB.Earth.x, save_path=OUTPUT_DIR / "jwst_rf_static.png")
+# plotTraj.Animation_RF(x,y,z,t,TB.r_12,TB.x_L2,TB.Earth.x, save_path=OUTPUT_DIR / "jwst_rf.mp4", fps=30, dpi=200)
+
+# # Transfer to Fixed Frame
+# XYZ = TB.RotToFixed(np.array([x,y,z]).T, TB.Omega, t)
+# x_fixed = XYZ[:,0]
+# y_fixed = XYZ[:,1]
+# z_fixed = XYZ[:,2]
+# x_Earth = 0.97*TB.r_12*np.cos(TB.Omega*t)
+# y_Earth = 0.97*TB.r_12*np.sin(TB.Omega*t)
+# z_Earth = np.zeros_like(x_Earth)
+
+# plotTraj.Animation_FF(x_fixed,y_fixed,z_fixed,x_Earth,y_Earth,z_Earth,t,TB.r_12, save_path=OUTPUT_DIR / "jwst_ff.mp4", fps=30, dpi=200)
+
+# # PYVISTA FOR VISUALIZATION
+# jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth, TB.r_12, number_of_years, 
+#                          jwstModelPath="./assets/models/JWST/scene.gltf", cubeMapPath='./assets/cubemaps/space',
+#                          save_movie=OUTPUT_DIR / "jwst_pv_fixed.mp4")
+
 from typing import Tuple
 from math import *
 import numpy as np
@@ -10,45 +89,40 @@ from pathlib import Path
 
 OUTPUT_DIR = Path("results/animation"); OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
 
-# Instantiate Three body system and its compositions. 
 TB = ThreeBodySystem()
 
-# Set the initial states for JWST in Rotating frame and nondimensional unit
-# Unstable Halo Orbit
-TB.JamesWebb.X0 = np.array([1.0062010416592476,             # x
-                            -9.8074369820712423e-16,       # xdot
-                            -2.1523078518259630e-23,       # y
-                            -1.3253477924660511e-2,        # ydot
-                            1.2380311201349303e-2,        # z
-                            -1.0956603459061133e-14])      # zdot
+TB.JamesWebb.X0 = np.array([
+    1.0062010416592476,
+    -2.1523078518259630e-23,
+    1.2380311201349303e-2,
+    -9.8074369820712423e-16,
+    -1.3253477924660511e-2,
+    -1.0956603459061133e-14
+])
 
-# Stable Halo Orbit
-# TB.JamesWebb.X0 = np.array([1.0065075924336142E+0,             # x
-#                             2.6228553664868397E-16,       # xdot
-#                             -2.1016745594119651E-23,       # y
-#                             -1.3729810632559413E-2,        # ydot
-#                             1.2329298254423008E-2,        # z
-#                             -2.4057058092832893E-15])      # zdot
-
-# Define parameters used in JWST dynamics
 JWST_params = {'pi_1':TB.pi_1,'pi_2':TB.pi_2}
 
-# Define the Input/Output System
-JWST_IO = ct.nlsys(TB.JamesWebb.JWST_update_nondim, TB.JamesWebb.JWST_output,
-                    states=['x','xdot','y','ydot','z','zdot'], name = 'JWST',
-                    inputs=['ux','uy','uz'], outputs=['x','xdot','y','ydot','z','zdot'],
-                    params=JWST_params)
+JWST_IO = ct.nlsys(
+    TB.JamesWebb.JWST_update_nondim, TB.JamesWebb.JWST_output,
+    states=['x','y','z','xdot','ydot','zdot'],
+    name='JWST',
+    inputs=['ux','uy','uz'],
+    outputs=['x','y','z','xdot','ydot','zdot'],
+    params=JWST_params
+)
 
-# Simulation
 number_of_years = 2
 timepts_year = 1000
-timepts = np.linspace(0,number_of_years*TB.year,number_of_years*timepts_year)       # Nondimensinal time points
+timepts = np.linspace(0,number_of_years*TB.year,number_of_years*timepts_year)
 u = [np.zeros_like(timepts),np.zeros_like(timepts),np.zeros_like(timepts)]
-response = ct.input_output_response(JWST_IO, timepts=timepts, inputs=u, initial_state=TB.JamesWebb.X0,
-                                    solve_ivp_kwargs={'rtol':1e-9,'atol':1e-12}, solve_ivp_method='RK45')
+
+response = ct.input_output_response(
+    JWST_IO, timepts=timepts, inputs=u, initial_state=TB.JamesWebb.X0,
+    solve_ivp_kwargs={'rtol':1e-9,'atol':1e-12}, solve_ivp_method='RK45'
+)
+
 time, outputs, inputs = response.time, response.outputs, response.inputs
 
-# Retrive JWST trajectories in rotation frame and dimentionalize them
 x = TB.r_12 * outputs['x']
 y = TB.r_12 * outputs['y']
 z = TB.r_12 * outputs['z']
@@ -57,22 +131,99 @@ ydot = TB.V_C * outputs['ydot']
 zdot = TB.V_C * outputs['zdot']
 t = TB.t_C * time
 
-# Plot the JWST Trajectory
-plotTraj.Plot_static_RF(x,y,z,t,TB.r_12,TB.x_L2,TB.Earth.x, save_path=OUTPUT_DIR / "jwst_rf_static.png")
-plotTraj.Animation_RF(x,y,z,t,TB.r_12,TB.x_L2,TB.Earth.x, save_path=OUTPUT_DIR / "jwst_rf.mp4", fps=30, dpi=200)
+plotTraj.Plot_static_RF(
+    x,y,z,t,TB.r_12,TB.x_L2,TB.Earth.x,
+    save_path=OUTPUT_DIR / "jwst_rf_static.png"
+)
 
-# Transfer to Fixed Frame
+plotTraj.Animation_RF(
+    x,y,z,t,TB.r_12,TB.x_L2,TB.Earth.x,
+    save_path=OUTPUT_DIR / "jwst_rf.mp4",
+    fps=30, dpi=200
+)
+
 XYZ = TB.RotToFixed(np.array([x,y,z]).T, TB.Omega, t)
 x_fixed = XYZ[:,0]
 y_fixed = XYZ[:,1]
 z_fixed = XYZ[:,2]
+
 x_Earth = 0.97*TB.r_12*np.cos(TB.Omega*t)
 y_Earth = 0.97*TB.r_12*np.sin(TB.Omega*t)
 z_Earth = np.zeros_like(x_Earth)
 
-plotTraj.Animation_FF(x_fixed,y_fixed,z_fixed,x_Earth,y_Earth,z_Earth,t,TB.r_12, save_path=OUTPUT_DIR / "jwst_ff.mp4", fps=30, dpi=200)
+plotTraj.Animation_FF(
+    x_fixed,y_fixed,z_fixed,
+    x_Earth,y_Earth,z_Earth,
+    t,TB.r_12,
+    save_path=OUTPUT_DIR / "jwst_ff.mp4",
+    fps=30, dpi=200
+)
 
-# PYVISTA FOR VISUALIZATION
-jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth, TB.r_12, number_of_years, 
-                         jwstModelPath="./assets/models/JWST/scene.gltf", cubeMapPath='./assets/cubemaps/space',
-                         save_movie=OUTPUT_DIR / "jwst_pv_fixed.mp4")
+jwstVisualizationFixed(
+    x_fixed, y_fixed, z_fixed,
+    x_Earth, y_Earth, z_Earth,
+    TB.r_12, number_of_years,
+    jwstModelPath="./assets/models/JWST/scene.gltf",
+    cubeMapPath='./assets/cubemaps/space',
+    save_movie=OUTPUT_DIR / "jwst_pv_fixed.mp4"
+)
+
+TB.JamesWebb.X0_LEO = np.array([
+    1.0002052721269323E+0,
+    -3.6778816842854946E-22,
+    -5.9605625108140006E-26,
+    -2.1985243974346765E-11,
+    -1.2128984679501061E-1,
+    4.4030232820030225E-23
+])
+
+timepts_leo = np.linspace(0, 1*TB.year, 1*timepts_year)
+
+time_LEO, outputs_LEO, inputs_LEO = TB.JamesWebb.JWST_propagate(
+    JWST_IO,
+    TB.JamesWebb.X0_LEO,
+    timepts=timepts_leo
+)
+
+x_LEO = TB.r_12*outputs_LEO['x']
+y_LEO = TB.r_12*outputs_LEO['y']
+z_LEO = TB.r_12*outputs_LEO['z']
+t_LEO = TB.t_C*time_LEO
+
+plotTraj.Plot_static_RF(
+    x_LEO,y_LEO,z_LEO,t_LEO,TB.r_12,TB.x_L2,TB.Earth.x,
+    save_path=OUTPUT_DIR / "leo_rf_static.png"
+)
+
+plotTraj.Animation_RF(
+    x_LEO,y_LEO,z_LEO,t_LEO,TB.r_12,TB.x_L2,TB.Earth.x,
+    save_path=OUTPUT_DIR / "leo_rf.mp4",
+    fps=30, dpi=200
+)
+
+XYZ_LEO = TB.RotToFixed(np.array([x_LEO,y_LEO,z_LEO]).T, TB.Omega, t_LEO)
+x_LEO_fixed = XYZ_LEO[:,0]
+y_LEO_fixed = XYZ_LEO[:,1]
+z_LEO_fixed = XYZ_LEO[:,2]
+
+x_Earth_LEO = 0.97*TB.r_12*np.cos(TB.Omega*t_LEO)
+y_Earth_LEO = 0.97*TB.r_12*np.sin(TB.Omega*t_LEO)
+z_Earth_LEO = np.zeros_like(x_Earth_LEO)
+
+plotTraj.Animation_FF(
+    x_LEO_fixed,y_LEO_fixed,z_LEO_fixed,
+    x_Earth_LEO,y_Earth_LEO,z_Earth_LEO,
+    t_LEO,TB.r_12,
+    save_path=OUTPUT_DIR / "leo_ff.mp4",
+    fps=30, dpi=200
+)
+
+jwstVisualizationFixed(
+    x_LEO_fixed, y_LEO_fixed, z_LEO_fixed,
+    x_Earth_LEO, y_Earth_LEO, z_Earth_LEO,
+    TB.r_12, 1,
+    jwstModelPath="./assets/models/JWST/scene.gltf",
+    cubeMapPath='./assets/cubemaps/space',
+    save_movie=OUTPUT_DIR / "leo_pv_fixed.mp4",
+    title="JWST LEO Orbit"
+)
--- src/usePyVista.py
+++ src/usePyVista.py
@@ -1,3 +1,173 @@
+# # PYVISTA FOR VISUALIZATION
+
+# import numpy as np
+# import pyvista as pv
+# from vtkmodules.vtkRenderingCore import vtkAssembly
+# import subprocess, shutil
+# from pathlib import Path
+
+# def jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth, r_12, number_of_years, 
+#                          jwstModelPath="./assets/models/JWST/scene.gltf", cubeMapPath='./assets/cubemaps',
+#                          save_movie=None, framerate=30, step=5, window_size=(1280, 720)):
+#     pv.global_theme.allow_empty_mesh = True
+
+#     num_stars = 40000
+#     spaceRadius = 10 * r_12
+#     starsCords = np.random.uniform(-spaceRadius, spaceRadius, size=(num_stars, 3))
+#     starPoints = pv.PolyData(starsCords)
+
+#     jwstOrbit = pv.PolyData()
+#     jwstSamples = len(x_fixed)
+
+#     earthOrbit = pv.PolyData()
+#     earthSamples = len(x_Earth)
+
+#     # REASONABLE SCALE FOR RADIUS BUT NOT IN SCALE
+
+#     sunRad = 0.1 * r_12
+#     earthRad = 0.05 * r_12
+
+#     sun = pv.Sphere(radius = sunRad, center = (0, 0, 0))
+#     earth = pv.Sphere(radius = earthRad * 0.3, center = (0, 0, 0))
+
+#     off = bool(save_movie)
+#     pl = pv.Plotter(window_size=window_size if save_movie else [3000, 2700], off_screen=off)
+#     pl.set_background('black')
+
+#     pl.import_gltf(jwstModelPath, set_camera = False)
+#     actors = pl.renderer.GetActors()
+#     actors.InitTraversal()
+#     jwstActors = []
+#     for _ in range(actors.GetNumberOfItems()):
+#         actor = actors.GetNextActor()
+#         jwstActors.append(actor)
+#     jwstAssembly = vtkAssembly()
+#     for actor in jwstActors:
+#         jwstAssembly.AddPart(actor)
+
+#     jwstAssembly.SetScale(0.001 * r_12)
+
+#     envTex = pv.cubemap(cubeMapPath, 'space-', '.png')
+#     pl.set_environment_texture(envTex)
+
+#     pl.add_mesh(jwstOrbit, color = 'cyan', line_width = 2, label = 'JWST Orbit')
+#     pl.add_mesh(earthOrbit, color = 'blue', line_width = 1, label = 'Earth Orbit')
+
+#     pl.add_mesh(sun, color='yellow', label='Sun')
+#     earthActor = pl.add_mesh(earth, color='deepskyblue', label='Earth')
+
+#     pl.add_mesh(starPoints, color='white', point_size=2 , render_points_as_spheres=True)
+
+#     light = pv.Light(
+#         position=(1, 1, 1),  
+#         positional=True,                   
+#         color='white',
+#         intensity=10.0,
+#         focal_point=(0, 0, 0)
+#     )
+#     pl.add_light(light)
+#     pl.renderer.ResetCameraClippingRange()
+
+#     pl.add_legend(bcolor=None, face=None, border=False, )
+#     pl.add_text(f"JWST Halo Orbit ({number_of_years} Years) - Fixed Frame",
+#                     position='upper_left', font_size=12, color='white')
+#     pl.add_actor(jwstAssembly)
+
+#     pl.add_axes()
+
+#     def callback(frame):
+#         earthPos = np.array([x_Earth[frame], y_Earth[frame], z_Earth[frame]])
+#         vecNorm = earthPos / np.linalg.norm(earthPos)
+#         cameraDistance = 1 * r_12
+#         cameraPos = earthPos + cameraDistance * vecNorm
+
+#         heightOffset = 0.5 * r_12
+#         cameraPos[2] += heightOffset
+
+#         freq = 2 * np.pi / 500
+#         phase = frame * freq
+
+#         up = np.array([0, 0, 1])
+#         right = np.cross(vecNorm, up)
+#         right /= np.linalg.norm(right)
+        
+#         forward = np.cross(right, vecNorm)
+        
+#         cameraPos += earthRad * 0.3 * np.sin(phase) * right
+#         cameraPos += earthRad * 0.3 * np.cos(phase) * forward
+
+#         cam = pl.renderer.GetActiveCamera()
+#         cam.SetPosition(*cameraPos)     
+#         cam.SetFocalPoint(0, 0, 0)    
+#         pl.renderer.ResetCameraClippingRange()
+
+#         lag = max(0, frame-200)
+#         xs = x_fixed[lag:frame]
+#         ys = y_fixed[lag:frame]
+#         zs = z_fixed[lag:frame]
+#         jwstOrbit.points = np.column_stack((xs, ys, zs))
+#         n = len(xs)
+#         if n > 1:
+#             lines = np.arange(0, n)
+#             lines = np.insert(lines, 0, n)
+#             jwstOrbit.lines = lines
+
+#         earthOrbit.points = np.column_stack((x_Earth[lag:frame], y_Earth[lag:frame], z_Earth[lag:frame]))
+#         nE = len(x_Earth[lag:frame])
+#         if nE > 1:
+#             lines = np.arange(0, nE)
+#             lines = np.insert(lines, 0, nE)
+#             earthOrbit.lines = lines
+
+#         jwstAssembly.SetPosition(x_fixed[frame], y_fixed[frame], z_fixed[frame])
+#         earthActor.position = [x_Earth[frame], y_Earth[frame], z_Earth[frame]]
+
+#         pl.render()
+    
+#     if save_movie:
+#         out = Path(save_movie)
+#         out.parent.mkdir(parents=True, exist_ok=True)
+#         tmp_dir = out.parent / (out.stem + "_frames")
+#         tmp_dir.mkdir(parents=True, exist_ok=True)
+
+#         # ensure ffmpeg exists
+#         assert shutil.which("ffmpeg"), "ffmpeg not found on PATH"
+
+#         # off-screen render for consistent frames
+#         pl.off_screen = True
+#         pl.window_size = window_size
+
+#         frame_ids = range(0, jwstSamples, max(1, step))
+#         for k, frame in enumerate(frame_ids):
+#             callback(frame)  # your existing update logic
+#             pl.render()
+#             # save PNG frame_000000.png, frame_000001.png, ...
+#             pl.screenshot(filename=str(tmp_dir / f"frame_{k:06d}.png"),
+#                         return_img=False, window_size=window_size)
+
+#         pl.close()
+
+#         # stitch PNGs -> MP4 (QuickTime-friendly)
+#         cmd = [
+#             "ffmpeg",
+#             "-y",
+#             "-framerate", str(framerate),
+#             "-i", str(tmp_dir / "frame_%06d.png"),
+#             "-vcodec", "libx264",
+#             "-pix_fmt", "yuv420p",
+#             "-movflags", "+faststart",
+#             str(out)
+#         ]
+#         subprocess.run(cmd, check=True)
+
+#         # optional: clean up frames
+#         for p in tmp_dir.glob("frame_*.png"):
+#             p.unlink()
+#         tmp_dir.rmdir()
+#     else:
+#         pl.add_timer_event(max_steps=jwstSamples, duration=500, callback=callback)
+#         pl.show()
+
 # PYVISTA FOR VISUALIZATION
 
 import numpy as np
@@ -6,9 +176,18 @@ from vtkmodules.vtkRenderingCore import vtkAssembly
 import subprocess, shutil
 from pathlib import Path
 
-def jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth, r_12, number_of_years, 
-                         jwstModelPath="./assets/models/JWST/scene.gltf", cubeMapPath='./assets/cubemaps',
-                         save_movie=None, framerate=30, step=5, window_size=(1280, 720)):
+def jwstVisualizationFixed(
+    x_fixed, y_fixed, z_fixed,
+    x_Earth, y_Earth, z_Earth,
+    r_12, number_of_years,
+    jwstModelPath="./assets/models/JWST/scene.gltf",
+    cubeMapPath='./assets/cubemaps',
+    save_movie=None,
+    framerate=30,
+    step=5,
+    window_size=(1280, 720),
+    title="JWST Orbit"         # <-- 唯一新增参数
+):
     pv.global_theme.allow_empty_mesh = True
 
     num_stars = 40000
@@ -22,19 +201,17 @@ def jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth,
     earthOrbit = pv.PolyData()
     earthSamples = len(x_Earth)
 
-    # REASONABLE SCALE FOR RADIUS BUT NOT IN SCALE
-
     sunRad = 0.1 * r_12
     earthRad = 0.05 * r_12
 
-    sun = pv.Sphere(radius = sunRad, center = (0, 0, 0))
-    earth = pv.Sphere(radius = earthRad * 0.3, center = (0, 0, 0))
+    sun = pv.Sphere(radius=sunRad, center=(0, 0, 0))
+    earth = pv.Sphere(radius=earthRad * 0.3, center=(0, 0, 0))
 
     off = bool(save_movie)
     pl = pv.Plotter(window_size=window_size if save_movie else [3000, 2700], off_screen=off)
     pl.set_background('black')
 
-    pl.import_gltf(jwstModelPath, set_camera = False)
+    pl.import_gltf(jwstModelPath, set_camera=False)
     actors = pl.renderer.GetActors()
     actors.InitTraversal()
     jwstActors = []
@@ -50,17 +227,17 @@ def jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth,
     envTex = pv.cubemap(cubeMapPath, 'space-', '.png')
     pl.set_environment_texture(envTex)
 
-    pl.add_mesh(jwstOrbit, color = 'cyan', line_width = 2, label = 'JWST Orbit')
-    pl.add_mesh(earthOrbit, color = 'blue', line_width = 1, label = 'Earth Orbit')
+    pl.add_mesh(jwstOrbit, color='cyan', line_width=2, label='JWST Orbit')
+    pl.add_mesh(earthOrbit, color='blue', line_width=1, label='Earth Orbit')
 
     pl.add_mesh(sun, color='yellow', label='Sun')
     earthActor = pl.add_mesh(earth, color='deepskyblue', label='Earth')
 
-    pl.add_mesh(starPoints, color='white', point_size=2 , render_points_as_spheres=True)
+    pl.add_mesh(starPoints, color='white', point_size=2, render_points_as_spheres=True)
 
     light = pv.Light(
-        position=(1, 1, 1),  
-        positional=True,                   
+        position=(1, 1, 1),
+        positional=True,
         color='white',
         intensity=10.0,
         focal_point=(0, 0, 0)
@@ -68,9 +245,13 @@ def jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth,
     pl.add_light(light)
     pl.renderer.ResetCameraClippingRange()
 
-    pl.add_legend(bcolor=None, face=None, border=False, )
-    pl.add_text(f"JWST Halo Orbit ({number_of_years} Years) - Fixed Frame",
-                    position='upper_left', font_size=12, color='white')
+    pl.add_legend(bcolor=None, face=None, border=False)
+    pl.add_text(
+        f"{title} ({number_of_years} Years) - Fixed Frame",    # <-- 修改点
+        position='upper_left',
+        font_size=12,
+        color='white'
+    )
     pl.add_actor(jwstAssembly)
 
     pl.add_axes()
@@ -90,18 +271,18 @@ def jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth,
         up = np.array([0, 0, 1])
         right = np.cross(vecNorm, up)
         right /= np.linalg.norm(right)
-        
+
         forward = np.cross(right, vecNorm)
-        
+
         cameraPos += earthRad * 0.3 * np.sin(phase) * right
         cameraPos += earthRad * 0.3 * np.cos(phase) * forward
 
         cam = pl.renderer.GetActiveCamera()
-        cam.SetPosition(*cameraPos)     
-        cam.SetFocalPoint(0, 0, 0)    
+        cam.SetPosition(*cameraPos)
+        cam.SetFocalPoint(0, 0, 0)
         pl.renderer.ResetCameraClippingRange()
 
-        lag = max(0, frame-200)
+        lag = max(0, frame - 200)
         xs = x_fixed[lag:frame]
         ys = y_fixed[lag:frame]
         zs = z_fixed[lag:frame]
@@ -123,31 +304,30 @@ def jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth,
         earthActor.position = [x_Earth[frame], y_Earth[frame], z_Earth[frame]]
 
         pl.render()
-    
+
     if save_movie:
         out = Path(save_movie)
         out.parent.mkdir(parents=True, exist_ok=True)
         tmp_dir = out.parent / (out.stem + "_frames")
         tmp_dir.mkdir(parents=True, exist_ok=True)
 
-        # ensure ffmpeg exists
         assert shutil.which("ffmpeg"), "ffmpeg not found on PATH"
 
-        # off-screen render for consistent frames
         pl.off_screen = True
         pl.window_size = window_size
 
         frame_ids = range(0, jwstSamples, max(1, step))
         for k, frame in enumerate(frame_ids):
-            callback(frame)  # your existing update logic
+            callback(frame)
             pl.render()
-            # save PNG frame_000000.png, frame_000001.png, ...
-            pl.screenshot(filename=str(tmp_dir / f"frame_{k:06d}.png"),
-                        return_img=False, window_size=window_size)
+            pl.screenshot(
+                filename=str(tmp_dir / f"frame_{k:06d}.png"),
+                return_img=False,
+                window_size=window_size
+            )
 
         pl.close()
 
-        # stitch PNGs -> MP4 (QuickTime-friendly)
         cmd = [
             "ffmpeg",
             "-y",
@@ -160,7 +340,6 @@ def jwstVisualizationFixed(x_fixed, y_fixed, z_fixed, x_Earth, y_Earth, z_Earth,
         ]
         subprocess.run(cmd, check=True)
 
-        # optional: clean up frames
         for p in tmp_dir.glob("frame_*.png"):
             p.unlink()
         tmp_dir.rmdir()
