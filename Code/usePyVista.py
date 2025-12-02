e94fb34a7b80b0ef11dcec604c2bdeb4edff714b
Mark Liu <markthezhang@gmail.com>
2025-12-02 00:38:03 -0500

    Setup pyVista fixed frame

--- Code/usePyVista.py
+++ Code/usePyVista.py
@@ -2,9 +2,22 @@
 
 import numpy as np
 import pyvista as pv
+import vtk
+import logging
 from vtkmodules.vtkRenderingCore import vtkAssembly
 import subprocess, shutil
 from pathlib import Path
+
+vtk.vtkObject.GlobalWarningDisplayOff()
+logging.getLogger().setLevel(logging.CRITICAL)
+
+def get_wobble(frame, intensity):
+    dx = np.sin(frame * 0.01) + np.sin(frame * 0.023) 
+    dy = np.cos(frame * 0.012) + np.sin(frame * 0.015)
+    dz = np.sin(frame * 0.009) + np.cos(frame * 0.02)
+    
+    vec = np.array([dx, dy, dz])
+    return vec * intensity
 
 def jwstVisualizationFixed(
     x_fixed, y_fixed, z_fixed,
@@ -21,7 +34,7 @@ def jwstVisualizationFixed(
     pv.global_theme.allow_empty_mesh = True
 
     num_stars = 40000
-    spaceRadius = 10 * r_12
+    spaceRadius = 10
     starsCords = np.random.uniform(-spaceRadius, spaceRadius, size=(num_stars, 3))
     starPoints = pv.PolyData(starsCords)
 
@@ -31,11 +44,25 @@ def jwstVisualizationFixed(
     earthOrbit = pv.PolyData()
     earthSamples = len(x_Earth)
 
-    sunRad = 0.1 * r_12
-    earthRad = 0.05 * r_12
+    sunRad = 0.1
+    earthRad = 0.02
 
-    sun = pv.Sphere(radius=sunRad, center=(0, 0, 0))
-    earth = pv.Sphere(radius=earthRad * 0.3, center=(0, 0, 0))
+    earth = pv.Sphere(center=(0,0,0), radius=earthRad, theta_resolution=120, phi_resolution=120, start_theta=270.001, end_theta=270,)
+    earth.active_texture_coordinates = np.zeros((earth.points.shape[0], 2))
+    for i in range(earth.points.shape[0]):
+        earth.active_texture_coordinates[i] = [
+            0.5 + np.arctan2(-earth.points[i, 0] / earthRad, earth.points[i, 1] / earthRad) / (2 * np.pi),
+            0.5 + np.arcsin(earth.points[i, 2] / earthRad) / np.pi,
+        ]
+    earth.translate((x_Earth[0] / r_12, y_Earth[0] / r_12, z_Earth[0] / r_12), inplace=True)
+
+    sun = pv.Sphere(center=(0,0,0), radius=sunRad, theta_resolution=120, phi_resolution=120, start_theta=270.001, end_theta=270,)
+    sun.active_texture_coordinates = np.zeros((sun.points.shape[0], 2))
+    for i in range(sun.points.shape[0]):
+        sun.active_texture_coordinates[i] = [
+            0.5 + np.arctan2(-sun.points[i, 0] / sunRad, sun.points[i, 1] / sunRad) / (2 * np.pi),
+            0.5 + np.arcsin(sun.points[i, 2] / sunRad) / np.pi,
+        ]
 
     off = bool(save_movie)
     pl = pv.Plotter(window_size=window_size if save_movie else [3000, 2700], off_screen=off)
@@ -52,28 +79,32 @@ def jwstVisualizationFixed(
     for actor in jwstActors:
         jwstAssembly.AddPart(actor)
         pl.remove_actor(actor)
-
-    jwstAssembly.SetScale(0.001 * r_12)
+    jwstAssembly.SetScale(0.0006)
 
     envTex = pv.cubemap(cubeMapPath, 'space-', '.png')
     pl.set_environment_texture(envTex)
+    earthTexture = pv.read_texture("./assets/textures/planets/earth.jpg")
+    sunTexture = pv.read_texture("./assets/textures/planets/sun.jpg")
+
+    # pl.add_mesh(floor, color='white', style = 'wireframe', show_edges=True, opacity=0.5, label="XY Plane")
 
     pl.add_mesh(jwstOrbit, color='cyan', line_width=2, label='JWST Orbit')
     pl.add_mesh(earthOrbit, color='blue', line_width=1, label='Earth Orbit')
 
-    pl.add_mesh(sun, color='yellow', label='Sun')
-    earthActor = pl.add_mesh(earth, color='deepskyblue', label='Earth')
+    sunActor = pl.add_mesh(sun, texture=sunTexture, smooth_shading = True)
+    earthActor = pl.add_mesh(earth, texture=earthTexture, smooth_shading=True)
 
     pl.add_mesh(starPoints, color='white', point_size=2, render_points_as_spheres=True)
 
     light = pv.Light(
-        position=(1, 1, 1),
+        position=(0, 0, 2),
         positional=True,
         color='white',
-        intensity=10.0,
+        intensity=2.5,
         focal_point=(0, 0, 0)
     )
     pl.add_light(light)
+    pl.renderer.ResetCameraClippingRange()
 
     pl.add_legend(bcolor=None, face=None, border=False)
     pl.add_text(
@@ -86,37 +117,36 @@ def jwstVisualizationFixed(
 
     pl.add_axes()
 
-    pl.renderer.ResetCameraClippingRange()
-    cameraPos = np.array([r_12 * 0.1, r_12 * 0.1, r_12 * 0.1])
-    cam = pl.renderer.GetActiveCamera()
-    cam.SetPosition(*cameraPos)
-    cam.SetFocalPoint(0, 0, 0)
     def callback(frame):
-        # earthPos = np.array([x_Earth[frame], y_Earth[frame], z_Earth[frame]])
-        # vecNorm = earthPos / np.linalg.norm(earthPos)
-        # cameraDistance = 1 * r_12
-        # cameraPos = earthPos + cameraDistance * vecNorm
+        earthPos = np.array([x_Earth[frame], y_Earth[frame], z_Earth[frame]])
+        vecNorm = earthPos / np.linalg.norm(earthPos)
+        cameraDistance = 0.7
+        cameraPos = earthPos + cameraDistance * vecNorm
 
-        # heightOffset = 0.5 * r_12
-        # cameraPos[2] += heightOffset
+        heightOffset = 0.2
+        cameraPos[2] += heightOffset
 
-        # freq = 2 * np.pi / 500
-        # phase = frame * freq
+        freq = 2 * np.pi / 500
+        phase = frame * freq
 
-        # up = np.array([0, 0, 1])
-        # right = np.cross(vecNorm, up)
-        # right /= np.linalg.norm(right)
+        up = np.array([0, 0, 1])
+        right = np.cross(vecNorm, up)
+        right /= np.linalg.norm(right)
 
-        # forward = np.cross(right, vecNorm)
+        forward = np.cross(right, vecNorm)
 
-        # cameraPos += earthRad * 0.3 * np.sin(phase) * right
-        # cameraPos += earthRad * 0.3 * np.cos(phase) * forward
+        cameraPos += earthRad * 0.3 * np.sin(phase) * right
+        cameraPos += earthRad * 0.3 * np.cos(phase) * forward
 
-
+        cam = pl.renderer.GetActiveCamera()
+        cam.SetPosition(*cameraPos)
+        cam.SetFocalPoint(0, 0, 0)
+        pl.renderer.ResetCameraClippingRange()
+        
         lag = max(0, frame - 200)
-        xs = x_fixed[lag:frame]
-        ys = y_fixed[lag:frame]
-        zs = z_fixed[lag:frame]
+        xs = x_fixed[lag:frame] / r_12
+        ys = y_fixed[lag:frame] / r_12
+        zs = z_fixed[lag:frame] / r_12
         jwstOrbit.points = np.column_stack((xs, ys, zs))
         n = len(xs)
         if n > 1:
@@ -130,8 +160,15 @@ def jwstVisualizationFixed(
             lines = np.arange(0, nE)
             lines = np.insert(lines, 0, nE)
             earthOrbit.lines = lines
+        dx = x_Earth[frame] - x_fixed[frame] / r_12
+        dy = y_Earth[frame] - y_fixed[frame] / r_12
+        dz = z_Earth[frame] - z_fixed[frame] / r_12
+        yaw = np.degrees(np.arctan2(dy, dx))
+        distance = np.sqrt(dx**2 + dy**2 + dz**2)
+        pitch = np.degrees(np.arcsin(dz / distance))
 
-        jwstAssembly.SetPosition(x_fixed[frame], y_fixed[frame], z_fixed[frame])
+        jwstAssembly.SetPosition(x_fixed[frame] / r_12, y_fixed[frame] / r_12, z_fixed[frame] / r_12)
+        jwstAssembly.SetOrientation(0, -pitch, yaw + 90)
         earthActor.position = [x_Earth[frame], y_Earth[frame], z_Earth[frame]]
 
         pl.render()
@@ -175,10 +212,13 @@ def jwstVisualizationFixed(
             p.unlink()
         tmp_dir.rmdir()
     else:
-        pl.add_timer_event(max_steps=jwstSamples, duration=500, callback=callback)
-        pl.show(cpos = cameraPos)
-
-
+        pl.show(interactive_update=True)
+        frame = 0
+        while frame < jwstSamples:
+            callback(frame) 
+            pl.update() 
+            frame += 1
+            
 def jwstVisualizationRot(
     x_JWST, y_JWST, z_JWST,
     x_Earth, y_Earth, z_Earth,
@@ -189,12 +229,14 @@ def jwstVisualizationRot(
     framerate=30,
     step=5,
     window_size=(1280, 720),
-    title="JWST Orbit"         # <-- 唯一新增参数
+    title="JWST Orbit",         # <-- 唯一新增参数
+    wobble_intensity = 0.001
+
 ):
     pv.global_theme.allow_empty_mesh = True
 
     num_stars = 40000
-    spaceRadius = 0.05
+    spaceRadius = 0.06
     starsCords = np.random.uniform(-spaceRadius, spaceRadius, size=(num_stars, 3))
     starsCords += np.array([x_Earth, y_Earth, z_Earth])
     starPoints = pv.PolyData(starsCords)
@@ -217,6 +259,8 @@ def jwstVisualizationRot(
     off = bool(save_movie)
     pl = pv.Plotter(window_size=window_size if save_movie else [3500, 2700], off_screen=off)
     pl.set_background('black')
+
+    pl.enable_shadows()
 
     pl.import_gltf(jwstModelPath, set_camera=False)
     actors = pl.renderer.GetActors()
@@ -244,10 +288,10 @@ def jwstVisualizationRot(
     pl.add_mesh(starPoints, color='white', point_size=2, render_points_as_spheres=True)
 
     light = pv.Light(
-        position=(0, 0, 0.09),
-        positional=True,
+        position=(x_Earth + 2, y_Earth + 10, 2.0),
+        positional=False,
         color='white',
-        intensity=10.0,
+        intensity=1.5,
         focal_point=(0, 0, 0)
     )
     pl.add_light(light)
@@ -286,7 +330,7 @@ def jwstVisualizationRot(
         pitch = np.degrees(np.arcsin(dz / distance))
         jwstAssembly.SetPosition(x_JWST[frame] / r_12, y_JWST[frame] / r_12, z_JWST[frame] / r_12)
         jwstAssembly.SetOrientation(0, -pitch, yaw + 90)
-   
+        pl.renderer.ResetCameraClippingRange()
 
     if save_movie:
         out = Path(save_movie)
@@ -303,8 +347,8 @@ def jwstVisualizationRot(
         cam = pl.renderer.GetActiveCamera()
         
         initial_cam_pos = np.array([0.5, 0.5, 0.5])
-        cam.SetPosition(*initial_cam_pos)
         cam.SetFocalPoint(x_Earth, y_Earth, z_Earth)
+        cam.SetPosition(*initial_cam_pos)
         pl.render() 
         pl.renderer.ResetCameraClippingRange()
 
@@ -313,6 +357,7 @@ def jwstVisualizationRot(
         zoom_steps = zoom_end - zoom_start
         target_pos_offset = np.array([-0.1, -0.1, -0.15])
         prev_progress = 0
+        new_pos = initial_cam_pos
 
         print(f"Rendering {jwstSamples} frames to {tmp_dir}...")
 
@@ -330,10 +375,12 @@ def jwstVisualizationRot(
                 jwstAssembly.SetScale(0.000003 + progress * 0.00003)
 
                 new_pos = initial_cam_pos + (target_pos_offset * progress)
-                cam.SetPosition(*new_pos)
 
                 pl.camera.zoom(0.1 ** delta_p)
                 pl.update() 
+
+            wobble = get_wobble(frame, intensity=wobble_intensity)
+            cam.SetPosition(*(new_pos + wobble))
 
             pl.renderer.ResetCameraClippingRange()
             pl.render()
@@ -397,9 +444,11 @@ def jwstVisualizationRot(
                 cam.SetFocalPoint(x_Earth, y_Earth, z_Earth + 0.0052 * progress)
                 jwstAssembly.SetScale(0.000003 + progress * 0.00003)
                 cameraPos = np.array([0.5 + (-0.1) * progress, 0.5 + (-0.1) * progress, 0.5 + (-0.15) * progress])
-                cam.SetPosition(*cameraPos)
                 pl.camera.zoom(0.1 ** deltaP)
                 current_duration = 0.001
+
+            wobble = get_wobble(frame, intensity=wobble_intensity)
+            cam.SetPosition(*(cameraPos + wobble))
             if frame == zoomEndFrame + 1:
                 current_duration = 0.001
             callback(frame)
